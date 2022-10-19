72c6f9d0fb9f461ff27c012db4213896c702de4d
amannivedh <115724247+amannivedh@users.noreply.github.com>
2022-10-19 22:20:58 +0530

    Add files via upload

---
+++ bmi_calc.py
@@ -0,0 +1,8 @@
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+a=float(height)
+b=int(weight)
+bmi=b/(a**2)
+print(bmi)
+BMI=int(bmi)
+print(f"BMI as a whole number {BMI}" )
