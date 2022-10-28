d258884ec0085f841d626ea2872dcb578f112524
amannivedh <115724247+amannivedh@users.noreply.github.com>
2022-10-28 22:40:18 +0530

    Add files via upload

---
+++ banker_roulette.py
@@ -0,0 +1,7 @@
+import random
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(",")
+num_items=len(names)
+random_choice=random.randint(0,num_items-1)
+person_pay=names[random_choice]
+print(person_pay +" is going to buy the meal today!")
