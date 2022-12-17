326955d673fc4dfbdd067dacdc9aad1e7d6821b7
amannivedh <115724247+amannivedh@users.noreply.github.com>
2022-12-17 16:33:27 +0530

    Add files via upload

---
+++ NATO alphabet/main.py
@@ -0,0 +1,7 @@
+import pandas
+
+data=pandas.read_csv("nato_phonetic_alphabet.csv")
+phonetic_dict={row.letter:row.code for(index,row) in data.iterrows()}
+word=input("enter a word: ").upper()
+list=[phonetic_dict[letter] for letter in word]
+print(list)
